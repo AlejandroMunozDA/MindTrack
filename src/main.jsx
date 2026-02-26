2078b65d061c14f2049d237d93b61770dc4ac612
Alejandro M <alejandromunozdelangel@gmail.com>
2026-02-25 22:46:43 -0600

    Generando la app para descargar en movil

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
     </React.StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('SW registrado: ', registration);
+        }).catch(registrationError => {
+            console.log('SW fallo al registrar: ', registrationError);
+        });
+    });
+}
