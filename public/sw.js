ae8597cc884e65bad975054e927129cae8a62251
Alejandro M <alejandromunozdelangel@gmail.com>
2026-02-25 23:09:01 -0600

    Correcion de error en toda la pagina

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,15 @@
-const CACHE_NAME = 'mindtrack-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html'
-];
+const CACHE_NAME = 'mindtrack-cache-v2';
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
     self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
+        fetch(event.request).catch(async () => {
+            const cache = await caches.open(CACHE_NAME);
+            return cache.match(event.request);
+        })
     );
 });
 
@@ -30,10 +17,10 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter(cacheName => {
-                    return cacheName !== CACHE_NAME;
-                }).map(cacheName => {
-                    return caches.delete(cacheName);
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
                 })
             );
         })
